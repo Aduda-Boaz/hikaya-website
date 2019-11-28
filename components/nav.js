7b053180bb3b7f70d19ebd4ef44b9be940d2a758
William Muli <willi.wambu@gmail.com>
2019-11-28 16:02:55 +0300

    Added nav section

--- components/nav.js
+++ components/nav.js
@@ -1,56 +1,72 @@
 import React from 'react'
-import Link from 'next/link'
-
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
-)
+  <section className="menu cid-rGcxRXmoaQ" once="menu" id="menu2-1">
+    <nav
+      className="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm box-shadow"
+    >
+      <button
+        className="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <div className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </button>
+      <div className="menu-logo">
+        <div className="navbar-brand">
+            <span className="navbar-logo">
+              <a href="https://hikaya.io">
+                <img
+                  src="assets/images/hikaya_logo.png"
+                  alt="Hikaya"
+                  title=""
+                />
+              </a>
+            </span>
+        </div>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div>
+          <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+            <li className="nav-item">
+              <a className="nav-link link nav-font" href="index.html#content4-3">
+                About</a
+              >
+            </li>
+            <li className="nav-item">
+              <a className="nav-link link nav-font" href="index.html#content4-8">
+                Products</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-buttons mbr-section-btn">
+          <a
+            className="btn btn-sm btn-primary-outline display-4 btn-nav"
+            href="https://activity.hikaya.app/accounts/login/"
+            target="_blank"
+          >
+            Login</a
+          >
+          <a
+            className="btn btn-sm btn-primary display-4 btn-nav"
+            href="index.html#content4-8"
+          >
+            Join</a
+          >
+        </div>
+      </div>
+    </nav>
+  </section>
+);
 
 export default Nav
