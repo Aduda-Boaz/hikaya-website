bc0f317a5b62f47e13418d3f35b8def65624b3ac
William Muli <willi.wambu@gmail.com>
2019-11-28 15:58:56 +0300

    Added components

---
+++ components/story/our-story.js
@@ -0,0 +1,25 @@
+const OurStory = () => (
+	<section className="mbr-section content4 cid-rGcAx6PWdw" id="content4-3">
+		<div className="container">
+			<div className="media-container-row">
+				<div className="title col-12 col-md-12">
+					<h2 className="align-center pb-3 mbr-fonts-style display-5">
+						Our story
+					</h2>
+					<h3
+						className="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-7"
+					>
+						Hikaya, which means <i>story</i> in Arabic works with nonprofit organizations to introduce a
+						set of integrated tools that re-engineer how data are used within
+						these organizations. By building open source solutions on common
+						data standards, we help nonprofits worldwide improve their
+						delivery of goods and services to those most in need while
+						making reporting to funders more transparent.
+					</h3>
+				</div>
+			</div>
+		</div>
+	</section>
+);
+
+export default OurStory;
