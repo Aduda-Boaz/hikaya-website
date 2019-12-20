e1bb7768d1d360f50851950956797a4c4641f8c5
Ninette <ninette.adhikari@gmail.com>
2019-12-20 21:03:12 +0100

    logo

--- components/Partners.js
+++ components/Partners.js
@@ -7,26 +7,34 @@ const Partners = () => {
         Our partners
       </h2>
       <div className='partner-container'>
-        <img
-          className='img-partner'
-          src='/assets/images/DO_Hatch_grey.png'
-          alt='digital ocean hatch logo'
-        />
-        <img
-          className='img-partner'
-          src='/assets/images/github-grey.png'
-          alt='github sponsors logo'
-        />
-        <img
-          className='img-partner segment'
-          src='/assets/images/segment_grey.png'
-          alt='segment logo'
-        />
-        <img
-          className='img-partner'
-          src='/assets/images/startup_school_grey.png'
-          alt='startup school logo'
-        />
+        <a href='https://www.digitalocean.com/hatch/' target='_blank'>
+          <img
+            className='img-partner'
+            src='/assets/images/DO_Hatch_grey.png'
+            alt='digital ocean hatch logo'
+          />
+        </a>
+        <a href='https://github.com/sponsors/hikaya-io' target='_blank'>
+          <img
+            className='img-partner'
+            src='/assets/images/github-grey.png'
+            alt='github sponsors logo'
+          />
+        </a>
+        <a href='https://segment.com/' target='_blank'>
+          <img
+            className='img-partner segment'
+            src='/assets/images/segment_grey.png'
+            alt='segment logo'
+          />
+        </a>
+        <a href='https://www.startupschool.org/' target='_blank'>
+          <img
+            className='img-partner'
+            src='/assets/images/startup_school_grey.png'
+            alt='startup school logo'
+          />
+        </a>
       </div>
     </div>
   );
