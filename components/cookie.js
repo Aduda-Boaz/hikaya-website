bc2eed636764f1eeec8cdf380814635e7e9019e8
William Muli <willi.wambu@gmail.com>
2020-01-07 12:49:45 +0300

    Change cookie component from functional to class component, use setState to show/hide the banner

--- components/cookie.js
+++ components/cookie.js
@@ -1,83 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-// export const Cookie = () => (
-//   <div className="cookie-banner" ng-class= "{'no-display': localStorage.getItem('cookieSeen') === 'shown'}">
-//     <p className="cookie-text">
-//       <img src="assets/images/cookie.png" width="16" className="cookie-image" />
-//       This website uses cookies to ensure you get the best experience.
-//       {/* <a href="/privacy" target="_blank"> Privacy Policy</a>  */}
-//     </p>
-//     <button
-//       className="btn btn-sm btn-primary display-6"
-//       onClick={acceptCookies}
-//     >
-//       Accept
-//     </button>
-//     <a
-//       href="/privacy"
-//       target="_blank"
-//       className="btn btn-sm btn-primary display-6"
-//       onClick={declineCookies}
-//     >
-//       Privacy Policy
-//     </a>
-//   </div>
-// );
+class Cookie extends Component {
+	COOKIE_SEEN_KEY = 'cookieSeen';
 
-function Cookie() {
-  if (!process.browser && localStorage.getItem('cookieSeen') !== 'shown') {
-    return (
-      <div className="cookie-banner">
-        <p className="cookie-text">
-          <img src="assets/images/cookie.png" width="16" className="cookie-image" />
-          This website uses cookies to ensure you get the best experience.
-          {/* <a href="/privacy" target="_blank"> Privacy Policy</a>  */}
-        </p>
-        <button
-          className="btn btn-sm btn-primary display-6"
-          onClick={acceptCookies}
-        >
-          Accept
-        </button>
-        <a
-          href="/privacy"
-          target="_blank"
-          className="btn btn-sm btn-primary display-6"
-          onClick={declineCookies}
-        >
-          Privacy Policy
-        </a>
-      </div>
-    )
-  }
-}
+	constructor(props) {
+		super(props);
+		this.state = {
+			show: false
+		};
 
-// componentDidMount() {
-//   const cookieSettings = localStorage.getItem('cookieSeen');
-//   console.log('cookieSettings : ', cookieSettings);
+	}
 
-//   if(cookieSettings) {
-//     return cookieSettings;
-//   } 
-//   return 'notSet';
-//   }
-  
-function acceptCookies() {
-  if (localStorage.getItem('cookieSeen') !== 'shown') {
-    $('.cookie-banner')
-      .delay(2000)
-      .fadeIn();
-    localStorage.setItem('cookieSeen', 'shown');
-  } 
-  $('.cookie-banner').hide({ duration: 1000 });
-}
+	acceptCookies = () => {
+		const cookieBanner = $('.cookie-banner');
+		if (localStorage.getItem(this.COOKIE_SEEN_KEY) !== 'shown') {
+			cookieBanner
+				.delay(2000)
+				.fadeIn();
+			localStorage.setItem(this.COOKIE_SEEN_KEY, 'shown');
+		}
+		cookieBanner.hide({duration: 1000});
+	};
 
-function declineCookies() {
-  // if (localStorage.getItem('cookieSeen') == 'shown') {
-  //   $('.cookie-banner').delay(2000).fadeIn();
-  //   localStorage.removeItem('cookieSeen')
-  // };
-  $('.cookie-banner').hide({ duration: 1000 });
+	declineCookies = () => {
+		$('.cookie-banner').hide({duration: 1000});
+	};
+
+	componentDidMount() {
+		const show = localStorage.getItem(this.COOKIE_SEEN_KEY) !== 'shown';
+		this.setState({ show })
+	}
+
+	render() {
+		return (
+			this.state.show ?
+				<div className="cookie-banner">
+					<p className="cookie-text">
+						<img src="assets/images/cookie.png" width="16" className="cookie-image"/>
+						This website uses cookies to ensure you get the best experience.
+					</p>
+					<button
+						className="btn btn-sm btn-primary display-6"
+						onClick={this.acceptCookies}
+					>
+						Accept
+					</button>
+					<a
+						href="/privacy"
+						target="_blank"
+						className="btn btn-sm btn-primary display-6"
+						onClick={this.declineCookies}
+					>
+						Privacy Policy
+					</a>
+				</div>
+				: null
+		);
+	}
 }
 
 export default Cookie;
