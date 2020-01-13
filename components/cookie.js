89136072c786f8d0beb1f4ffdf01d4183badd338
Ninette <ninette.adhikari@gmail.com>
2020-01-13 20:55:14 +0100

    added return function in cookie

--- components/cookie.js
+++ components/cookie.js
@@ -1,28 +1,58 @@
 import React from 'react';
 
-export const Cookie = () => (
-  <div className='cookie-banner'>
-    <p className='cookie-text'>
-      <img src='assets/images/cookie.png' width='16' className='cookie-image' />
-      This website uses cookies to ensure you get the best experience.
-      {/* <a href="/privacy" target="_blank"> Privacy Policy</a>  */}
-    </p>
-    <button
-      className='btn btn-sm btn-primary display-6'
-      onClick={acceptCookies}
-    >
-      Accept
-    </button>
-    <a
-      href='/privacy'
-      target='_blank'
-      className='btn btn-sm btn-primary display-6'
-      onClick={declineCookies}
-    >
-      Privacy Policy
-    </a>
-  </div>
-);
+const Cookie = () => {
+  return (
+    <div className='cookie-banner'>
+      <p className='cookie-text'>
+        <img
+          src='assets/images/cookie.png'
+          width='16'
+          className='cookie-image'
+        />
+        This website uses cookies to ensure you get the best experience.
+        {/* <a href="/privacy" target="_blank"> Privacy Policy</a>  */}
+      </p>
+      <button
+        className='btn btn-sm btn-primary display-6'
+        onClick={acceptCookies}
+      >
+        Accept
+      </button>
+      <a
+        href='/privacy'
+        target='_blank'
+        className='btn btn-sm btn-primary display-6'
+        onClick={declineCookies}
+      >
+        Privacy Policy
+      </a>
+    </div>
+  );
+};
+
+// export const Cookie = () => (
+//   <div className='cookie-banner'>
+//     <p className='cookie-text'>
+//       <img src='assets/images/cookie.png' width='16' className='cookie-image' />
+//       This website uses cookies to ensure you get the best experience.
+//       {/* <a href="/privacy" target="_blank"> Privacy Policy</a>  */}
+//     </p>
+//     <button
+//       className='btn btn-sm btn-primary display-6'
+//       onClick={acceptCookies}
+//     >
+//       Accept
+//     </button>
+//     <a
+//       href='/privacy'
+//       target='_blank'
+//       className='btn btn-sm btn-primary display-6'
+//       onClick={declineCookies}
+//     >
+//       Privacy Policy
+//     </a>
+//   </div>
+// );
 
 function acceptCookies() {
   if (localStorage.getItem('cookieSeen') != 'shown') {
