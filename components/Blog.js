d2e1bf440eab245ca42e556a712616972c0256f9
Ninette <ninette.adhikari@gmail.com>
2020-02-10 22:15:06 +0100

    blog text

--- components/Blog.js
+++ components/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
   return (
     <div className='blog-page'>
       <h1 className='display-5'>Blog</h1>
-      <p className="blog-text">We are coming soon with exciting topics! 😊</p>
+      <p className='blog-text'>We are coming soon with exciting topics! 😊</p>
     </div>
   );
 };
